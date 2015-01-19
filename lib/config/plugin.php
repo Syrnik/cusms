ff27293c1a46b4b82183ec4810faf125bf41b82a
Serge Rodovnichenko <sergerod@gmail.com>
2015-01-20 02:44:26 +0300

    Initial commit

---
+++ lib/config/plugin.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'name'     => 'SMS to customer',
+    'icon'     => 'img/cusms.gif',
+    'version'  => '1.0.0',
+    'vendor'   => '670917',
+    'handlers' =>
+        array(),
+);
---
+++ lib/shopCusmsPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopCusmsPlugin extends shopPlugin
+{
+
+}
