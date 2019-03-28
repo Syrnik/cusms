b12ee0a6ebf57c0c3e21a78741182fb282956f67
Serge Rodovnichenko <sergerod@gmail.com>
2019-03-28 03:30:10 +0300

    Fix config

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,10 +1,10 @@
 <?php
 return array(
-    'name'          => _wp('SMS to customer'),
+    'name'          => 'SMS to customer',
     'img'           => 'img/sms_icon.png',
     'version'       => '1.1.0',
     'vendor'        => '670917',
-    'shop_settings' => FALSE,
+    'shop_settings' => false,
     'handlers'      =>
         array(
             'backend_order' => 'backendOrderHandler'
